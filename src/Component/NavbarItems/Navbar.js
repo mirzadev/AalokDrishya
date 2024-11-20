f7538d13d8166c37cd34e0783ad6754acf35f83a
Mirza Abdul Awal <84652300+mirzadev@users.noreply.github.com>
2024-11-19 22:08:00 -0500

    Navbar Menu updated

--- src/Component/NavbarItems/Navbar.js
+++ src/Component/NavbarItems/Navbar.js
@@ -1,25 +1,19 @@
 import React, { useState } from "react";
 import AD_Logo from "../../Assets/Cover/AalokDrishty_Logo.png";
 import "./NavbarStyles.css";
-import { MenuItems, MenuItems1, MenuItems2, MenuItems3 } from "./MenuItems";
+import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 import Dropdown from "../Drop-Down-Menu/ActivityDropDown";
-import Dropdown1 from "../Drop-Down-Menu/ActivityDropDown1";
-import Dropdown2 from "../Drop-Down-Menu/ActivityDropDown2";
-import Dropdown3 from "../Drop-Down-Menu/ActivityDropDown2";
 
 function Navbar() {
   {
     const [dropdown, setDropdown] = useState(false);
-    const [dropdown1, setDropdown1] = useState(false);
-    const [dropdown2, setDropdown2] = useState(false);
-    const [dropdown3, setDropdown3] = useState(false);
     const [clicked, setClicked] = useState(false);
     return (
       <nav className="NavbarItems">
         <Link data-testid="link" to="/">
           <span>
-            <img id="GCH-logo" src={AD_Logo}></img>
+            <img id="AD-logo" src={AD_Logo}></img>
           </span>
         </Link>
 
@@ -28,7 +22,7 @@ function Navbar() {
         </div>
         <ul className={clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
-            if (item.title === "Commercial") {
+            if (item.title === "Work") {
               return (
                 <li
                   key={item.id}
@@ -41,90 +35,6 @@ function Navbar() {
                     <i className={item.ddIcon} id="activity-drop-menu"></i>
                   </Link>
                   {dropdown && <Dropdown />}
-                </li>
-              );
-            }
-
-            return (
-              <li key={index}>
-                <Link className={item.CName} to={item.url}>
-                  <i className={item.icon}></i>
-                  {item.title}
-                  <i className={item.ddIcon}></i>
-                </Link>
-              </li>
-            );
-          })}
-          {MenuItems1.map((item, index) => {
-            if (item.title === "Portraits") {
-              return (
-                <li
-                  key={item.id}
-                  className={item.CName}
-                  onMouseEnter={() => setDropdown1(true)}
-                  onMouseLeave={() => setDropdown1(false)}
-                >
-                  <Link id="activity-menu" to={item.url}>
-                    <i className={item.icon}></i> {item.title}
-                    <i className={item.ddIcon} id="activity-drop-menu"></i>
-                  </Link>
-                  {dropdown1 && <Dropdown1 />}
-                </li>
-              );
-            }
-
-            return (
-              <li key={index}>
-                <Link className={item.CName} to={item.url}>
-                  <i className={item.icon}></i>
-                  {item.title}
-                  <i className={item.ddIcon}></i>
-                </Link>
-              </li>
-            );
-          })}
-          {MenuItems2.map((item, index) => {
-            if (item.title === "Food") {
-              return (
-                <li
-                  key={item.id}
-                  className={item.CName}
-                  onMouseEnter={() => setDropdown2(true)}
-                  onMouseLeave={() => setDropdown2(false)}
-                >
-                  <Link id="activity-menu" to={item.url}>
-                    <i className={item.icon}></i> {item.title}
-                    <i className={item.ddIcon} id="activity-drop-menu"></i>
-                  </Link>
-                  {dropdown2 && <Dropdown2 />}
-                </li>
-              );
-            }
-
-            return (
-              <li key={index}>
-                <Link className={item.CName} to={item.url}>
-                  <i className={item.icon}></i>
-                  {item.title}
-                  <i className={item.ddIcon}></i>
-                </Link>
-              </li>
-            );
-          })}
-          {MenuItems3.map((item, index) => {
-            if (item.title === "Landscape") {
-              return (
-                <li
-                  key={item.id}
-                  className={item.CName}
-                  onMouseEnter={() => setDropdown3(true)}
-                  onMouseLeave={() => setDropdown3(false)}
-                >
-                  <Link id="activity-menu" to={item.url}>
-                    <i className={item.icon}></i> {item.title}
-                    <i className={item.ddIcon} id="activity-drop-menu"></i>
-                  </Link>
-                  {dropdown3 && <Dropdown3 />}
                 </li>
               );
             }

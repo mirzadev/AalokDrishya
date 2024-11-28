2f99d65f6745c81485074a641eb9bc24a5e930aa
Mirza Abdul Awal <84652300+mirzadev@users.noreply.github.com>
2024-11-27 22:34:29 -0500

    About Us Page Map loc added

--- src/Component/About/AboutUs.js
+++ src/Component/About/AboutUs.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./AboutUsStyles.css";
-import President_AD from "../../Assets/About/AnamikaChowdhury.jpg";
-import VP_AD from "../../Assets/About/AnamikaChowdhury.jpg";
+import ekCupChaIntro from "../../Assets/About/ekCupCha-1.jpg";
+import MapComponent from "./MapLocation";
+
+// import President_AD from "../../Assets/About/AnamikaChowdhury.jpg";
+// import VP_AD from "../../Assets/About/AnamikaChowdhury.jpg";
+
 import { Link } from "react-router-dom";
 function AboutUs() {
   return (
@@ -88,6 +92,65 @@ function AboutUs() {
               <span className="fa solid alt fa-envelope"></span>
             </Link>
           </div>
+        </div>
+      </div>
+      <div className="life_at_AD">
+        <h1>LIFE AT AALOK DRISHYA</h1>
+      </div>
+      <div className="ekCupCha-intro-image">
+        <a href="https://www.youtube.com/watch?v=FI9_ft95mgA" target="_blank">
+          <img id="ekCupCha-image" alt="image-vdo" src={ekCupChaIntro} />
+        </a>
+      </div>
+      <div className="Official_Contact">
+        <div className="life_at_AD">
+          <h1>CONTACT US</h1>
+        </div>
+        <div className="ContactInfo">
+          <div className="office-loc">
+            <div
+              className="fa-solid fa-thumbtack"
+              style={{ fontSize: "30px" }}
+            ></div>
+            <h2>Our Office</h2>
+            <Link
+              to="https://www.google.com/maps/place/512+Shady+Pine+Way,+Greenacres,+FL+33415/@26.6398988,-80.1460499,17z/data=!3m1!4b1!4m6!3m5!1s0x88d9262ad0671287:0xde974d1673268148!8m2!3d26.639894!4d-80.143475!16s%2Fg%2F11cscj25n8?entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>512 Shady Pine Way,</div>
+              <div>Greenacres, FL-33415</div>
+            </Link>
+          </div>
+          <div className="tel-contact">
+            <div
+              className="fa-solid fa-phone"
+              style={{ fontSize: "30px" }}
+            ></div>
+            <h2>Phone</h2>
+            <div className="phoneNumber">1(561) 999 9999 (Cell)</div>
+            <div className="phoneNumber">1(561) 999 9999 (Office)</div>
+          </div>
+          <div className="email-contact">
+            <div
+              className="fa-solid fa-envelope"
+              style={{ fontSize: "30px" }}
+            ></div>
+            <h2>Email</h2>
+            <Link
+              to="mailto:qazirabbani@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="email-address">qazirabbani@gmail.com</p>
+            </Link>
+          </div>
+        </div>
+        <div className="life_at_AD">
+          <h1>ON YOUR MAP</h1>
+        </div>
+        <div className="map-location">
+          <MapComponent />
         </div>
       </div>
     </div>

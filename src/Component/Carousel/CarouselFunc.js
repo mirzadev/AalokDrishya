f58917b29aae6ccd5cbf2440c0ca52e917a7cc7b
Mirza Abdul Awal <84652300+mirzadev@users.noreply.github.com>
2025-01-01 16:05:23 -0500

    footer items adjusted

--- src/Component/Carousel/CarouselFunc.js
+++ src/Component/Carousel/CarouselFunc.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./CarouselFuncStyle.css";
 import img1 from "../../Assets/HomePageImg/SlidingImage-1.jpg";
 import img2 from "../../Assets/HomePageImg/SlidingImage-2.jpg";
 import img3 from "../../Assets/HomePageImg/SlidingImage-3.jpg";
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,16 +2,14 @@ import Navbar from "../Component/NavbarItems/Navbar";
 import HomeItems from "../Component/HomePage/HomeItems";
 import FeaturedAlbum from "../Component/HomeFeature/FeaturedAlbum";
 import Footer from "../Component/Footer/Footer";
-// import Carousel from "../Component/CorouselItems/Carousel";
-import Carousel1 from "../Component/Carousel/CarouselFunc";
-// import slides from "../Component/CorouselItems/CarouselMenu";
+import Carousel from "../Component/Carousel/CarouselFunc";
 import "../Component/HomePage/HomeStyle.css";
 
 function Home() {
   return (
     <>
       <Navbar />
-      <Carousel1 />
+      <Carousel />
       <HomeItems />
       <FeaturedAlbum />
       <Footer />
